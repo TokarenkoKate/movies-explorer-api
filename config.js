66f8651773a636b0a3d1ca082c82bfcc4ca722a0
Kate Tokarenko <trinatiani@yandex.ru>
2023-04-16 17:42:02 +0300

    12. Поменять порт

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const { JWT_SECRET = 'JTW_SECRET' } = process.env;
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const { DB_ADDRESS = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 const { NODE_ENV = 'production' } = process.env;
 
