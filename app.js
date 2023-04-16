5998c79559151457b8b861bb752a2f3517e38433
Kate Tokarenko <trinatiani@yandex.ru>
2023-04-16 15:54:51 +0300

    3. Подключение к БД и классы ошибок.
    В App.js настроено подключение к БД MongoDB.
    Добавлены классы ошибок и подключен централизованный обработчик ошибок.

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const cors = require('cors');
+const { errors } = require('celebrate');
+const errorHandler = require('./middlewares/error-handler');
+const { PORT, DB_ADDRESS } = require('./config');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(DB_ADDRESS, {
+  useNewUrlParser: true,
+});
+
+app.use(helmet());
+
+// app.use(routes);
+
+app.use(errors());
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(DB_ADDRESS);
+});
