66585bc4ff3e676852aba1fff571db1af1732fc7
Kate Tokarenko <trinatiani@yandex.ru>
2023-04-16 15:56:58 +0300

    4. Добавлены модели для user, movie

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Поле "name" должно быть заполнено.'],
+    default: 'Мария',
+    minLength: [2, 'Имя пользователя должно содержать минимум 2 символа.'],
+    maxLength: [30, 'Имя пользователя должно содержать максимум 30 символов.'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Поле "email" должно быть заполнено.'],
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Некорректный email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Поле "password" должно быть заполнено.'],
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
